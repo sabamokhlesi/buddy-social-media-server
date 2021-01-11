d1b3f87b85310c16ea23c2b9b570e3a8b38edd04
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2021-01-11 08:36:25 -0500

    deleted some commented lines

--- app.js
+++ app.js
@@ -12,15 +12,6 @@ const feedRoutes = require('./routes/feed')
 
 const app = express();
 
-// const fileStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'images');
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, new Date().toISOString() + '-' + file.originalname);
-//   }
-// });
-
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
