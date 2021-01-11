964890d192e2054cb82282cc43a79495c3d55b3f
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2021-01-11 09:26:19 -0500

    added timestamp for new users

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {type: String,required: true},
   password:{type: String,required: true},
+  createdAt: {type:Date, default:Date.now},
   userInfo:{
     userName:{type:String,required:true},
     name:{type: String},
